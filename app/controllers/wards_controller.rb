7a6a81d8039e7be171196a6fa59b77e361e119d7
maxwell wong <midorikawamaxwell@gmail.com>
2023-02-11 14:27:52 +0900

    created the actions for wards controller

--- app/controllers/wards_controller.rb
+++ app/controllers/wards_controller.rb
@@ -1,2 +1,11 @@
 class WardsController < ApplicationController
+  skip_before_action :authenticate_user
+
+  def index
+    @wards = Ward.all
+  end
+
+  def show
+    @ward = Ward.find(param[:id])
+  end
 end
