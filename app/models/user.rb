6b270605dff6ec910e43952198ccc16d90672919
maxwell wong <midorikawamaxwell@gmail.com>
2023-02-11 13:14:51 +0900

    destory

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :reviews, dependent: :destory
+  has_many :reviews, dependent: :destroy
   has_many :user_wards
 end
