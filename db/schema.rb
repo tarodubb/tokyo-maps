adfa7bd213f846a4e475a4a3bf0a1eedfa053c72
maxwell wong <midorikawamaxwell@gmail.com>
2023-02-11 14:13:53 +0900

    temporary nav and footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_125146) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_11_014925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_wards", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
