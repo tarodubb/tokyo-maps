dd9bfc63ef681c3ee9ade95916f849daa522e935
maxwell wong <midorikawamaxwell@gmail.com>
2023-02-18 15:35:33 +0900

    re adding flyto file

---
+++ app/helpers/flyto.rb
@@ -0,0 +1,27 @@
+<body>
+<div id="container">
+<button id="fly-to-tokyo">Fly to Tokyo</button>
+<button id="reset-view">Reset</button>
+</div>
+</body>
+
+
+
+
+# //fly to Tokyo/add after the map loads
+document.querySelector('#fly-to-tokyo').addEventListener('click', () => {
+  this.map.flyTo({
+    center: [139.697888, 35.685098],
+    zoom: 10,
+    pitch: 45
+  })
+})
+
+# //reset the map view
+document.querySelector('#reset-view').addEventListener('click', () => {
+  this.map.flyTo({
+    center: INITIAL_CENTER,
+    zoom: INITIAL_ZOOM,
+    pitch: 0
+  })
+})
