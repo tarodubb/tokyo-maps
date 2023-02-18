b7cf0bb79ca9fcc159605422212a9732b8a27a5d
Tim Chapin <chapin.timothys@gmail.com>
2023-02-18 09:02:10 +0900

    User seed file and added refs to reviews and UserWard table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,11 @@
 require "json"
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-# Ward.destroy_all
-# p "Destroyed all wards"
+require "faker"
+require 'securerandom'
+# I18n.reload!
+p "Destroying Wards"
 Ward.destroy_all
+p "Destroying Users"
+User.destroy_all
 
 file_path = File.join(File.dirname(__FILE__), "./tokyo.geojson")
 parsed_geo_json = JSON.parse(File.read(file_path))
@@ -68,5 +65,30 @@ max_ward = Ward.create(name: "Shibuya",
             population_density: 15000)
 
 max_ward.save!
-
 p "Max seeded"
+
+# User seeding
+p "Seeding users"
+# Function to generate a random string of lowercase letters and digits
+def generate_password
+  SecureRandom.alphanumeric(8)
+end
+# Generate 100 users
+10.times do
+  first_name = Faker::Name.unique.first_name
+  last_name = Faker::Name.last_name
+  email = "#{first_name.downcase}.#{last_name.downcase}@yahoo.com"
+  password = generate_password
+  username = "#{first_name.downcase}.#{last_name.downcase}"
+  ward = "Shibuya"
+  address = "#{ward}, Tokyo"
+  user = User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    password: password,
+    username: username,
+    address: address
+  )
+  p user
+end
