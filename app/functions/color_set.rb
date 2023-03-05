225c9c5862145baedc4f34526d84626647f2198f
Tim Chapin <chapin.timothys@gmail.com>
2023-03-05 20:59:11 +0900

    Added custom color method and cleaned up Map controller

--- app/functions/color_set.rb
+++ app/functions/color_set.rb
@@ -1,34 +1,57 @@
 require_relative "./extrusion_height"
 class ColorSet
-  def self.color_set(category)
+  @max_r = 224
+  @max_g = 77
+  @max_b = 42
+  @min_r = 1
+  @min_g = 1
+  @min_b = 1
+  def self.color_set(avg_rent_max_array, avg_rent_min_array, safety_array)
     # Set main color (The higher the number the darker the color)
-    max_red = 59
-    max_green = 57
-    max_blue = 208
-
-    lower_red = 1
-    lower_green = 1
-    lower_blue = 1
     # Set hover color
     hover_red = 255
     hover_green = 255
     hover_blue = 255
 
+    min_1ldk = avg_rent_min_array[0]
+    min_2ldk = avg_rent_min_array[1]
+    min_3ldk = avg_rent_min_array[2]
+
+    max_1ldk = avg_rent_max_array[0]
+    max_2ldk = avg_rent_max_array[1]
+    max_3ldk = avg_rent_max_array[2]
+
+    max_safety = safety_array[0]
+    min_safety = safety_array[1]
 
     wards_geojson = File.read("public/tokyo.geojson")
     wards_parsed_geojson = JSON.parse(wards_geojson)
-    wards_parsed_geojson["features"].each do |feature|
-      ward_1ldk_height = feature["properties"]["one_ldk_sort_height"]
-      red_color_dif = (ward_1ldk_height * max_color_r)
-      green_color_dif = (ward_1ldk_height * max_color_g)
-      blue_color_dif = (ward_1ldk_height * max_color_b)
 
-      # p base_color.to_rgb
-      feature["properties"]["base-color"] = "rgb(#{red},#{green},#{blue})"
-      feature["properties"]["base-color"] =
-        "rgb(#{red - red_color_dif.round},#{green - green_color_dif.round},#{blue - blue_color_dif.round})"
-      feature["properties"]["hover-color"] = "rgb(#{hover_red},#{hover_green},#{hover_blue})"
+    wards_parsed_geojson["features"].each do |feature|
+      ward_1ldk = feature["properties"]["one_ldk_sort_height"]
+      ward_2ldk = feature["properties"]["two_ldk_sort_height"]
+      ward_3ldk = feature["properties"]["three_ldk_sort_height"]
+      ward_safety = feature["properties"]["safety"]
+      # 1ldk + safety
+      feature["properties"]["one_ldk_sort_color"] = rgb(ward_1ldk, max_1ldk, min_1ldk)
+      feature["properties"]["one_ldk_safety_color"] = rgb(ward_safety, max_safety, min_safety)
+      # 2ldk + safety
+      feature["properties"]["two_ldk_sort_color"] = rgb(ward_2ldk, max_2ldk, min_2ldk)
+      feature["properties"]["two_ldk_safety_color"] = rgb(ward_safety, max_safety, min_safety)
+      # 3ldk + safety
+      feature["properties"]["three_ldk_sort_color"] = rgb(ward_3ldk, max_3ldk, min_3ldk)
+      feature["properties"]["three_ldk_safety_color"] = rgb(ward_safety, max_safety, min_safety)
+      # safety
+      feature["properties"]["safety"] = rgb(ward_safety, max_safety, min_safety)
     end
     File.write('public/tokyo.geojson', JSON.pretty_generate(wards_parsed_geojson))
+  end
+
+  def self.rgb(sort_val, max, min)
+    normalized_val = (sort_val.to_f - min).fdiv(max - min)
+    red = @max_r - ((@max_r - @min_r) * normalized_val)
+    green = @max_g - ((@max_g - @min_g) * normalized_val)
+    blue = @max_b - ((@max_b - @min_b) * normalized_val)
+    "rgb(#{red.round.abs},#{green.round.abs},#{blue.round.abs})"
   end
 end
